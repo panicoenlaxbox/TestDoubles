dbca3f94a11b360dd2de530ebcb632441942cb1d
panicoenlaxbox <panicoenlaxbox@gmail.com>
2020-03-04 12:03:06 +0100

    create_a_mock

--- TestDoubles/UnitTest1.cs
+++ TestDoubles/UnitTest1.cs
@@ -1,14 +1,47 @@
 using System;
 using Xunit;
+using Moq;
+using FluentAssertions;
 
 namespace TestDoubles
 {
-    public class UnitTest1
+    public class MoqShould
     {
         [Fact]
-        public void Test1()
+        public void create_a_mock()
         {
+            var foo = new Mock<IFoo>();
+            
+            foo.Object.DoSomething("foo").Should().BeFalse();
 
+            foo.Setup(f=>f.DoSomething("foo")).Returns(true);
+            
+            foo.Object.DoSomething("foo").Should().BeTrue();
         }
+    }
+
+    public interface IFoo
+    {
+        Bar Bar { get; set; }
+        string Name { get; set; }
+        int Value { get; set; }
+        bool DoSomething(string value);
+        bool DoSomething(int number, string value);
+        string DoSomethingStringy(string value);
+        bool TryParse(string value, out string outputValue);
+        bool Submit(ref Bar bar);
+        int GetCount();
+        bool Add(int value);
+    }
+
+    public class Bar
+    {
+        public virtual Baz Baz { get; set; }
+        public virtual bool Submit() { return false; }
+    }
+
+    public class Baz
+    {
+        public virtual string Name { get; set; }
     }
 }
